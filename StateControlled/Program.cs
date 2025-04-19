e8d6760b997845d2fc9b3160519a32ecc5ae7487
maunatlalatechinc <137209654+maunatlalatechinc@users.noreply.github.com>
2025-04-19 21:44:08 +0200

    The first version of the statecontrolled loop code

---
+++ StateControlled/Program.cs
@@ -0,0 +1,55 @@
+﻿//Temoso Nare
+using System;
+//using System.Windows.Forms;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StateControlledLoops
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter the students marks one after another.");
+
+            int n = 0;
+            int iSum = 0;
+            int iMark = 0;
+
+
+            while (IsAnotherOne())
+            {
+                n++;
+                Console.Write("Mark " + n.ToString() + ":");
+                iMark = int.Parse(Console.ReadLine());
+                iSum += iMark;
+            }
+
+            if (n > 0)
+            {
+                double dAverage = iSum / (n * 1.0);
+                Console.WriteLine("The average mark is " + dAverage.ToString("F1"));
+            }
+            else
+            {
+                Console.WriteLine("No mark was entered !!");
+            }
+
+            Console.WriteLine("Press any key to exit !!");
+            Console.ReadKey();
+        }
+        static bool IsAnotherOne()
+        {
+            //return MessageBox.Show("Another mark (Y/N)?", "Marks", MessageBoxButtons.YesNo, MessageBoxIcon.Question) == DialogResult.Yes;
+            
+            Console.Write("Another Mark (Y/N) ? :");
+            bool isAnotherOne = Console.ReadKey().KeyChar.ToString().ToUpper() == "Y";
+            Console.WriteLine();
+            return isAnotherOne;
+            
+
+        }
+    }
+}
